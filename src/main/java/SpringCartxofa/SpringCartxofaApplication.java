4242370afd8ec72f16754bbb82b30d04c55f2a13
@Arielus <llugo.vera.ll@gmail.com>
2022-11-19 12:51:52 +0100

    Commit en sobremesa

--- src/main/java/SpringCartxofa/SpringCartxofaApplication.java
+++ src/main/java/SpringCartxofa/SpringCartxofaApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringCartxofaApplication {
+	//EN SOBREMESA
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCartxofaApplication.class, args);
 	}
